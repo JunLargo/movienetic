8ce959e5bb8310dbab632aecdb35836cc71f9ea1
The Noisy Duck <thenoisyduck@Thes-MacBook-Pro.local>
2022-03-18 22:33:41 +0800

    adding new movie details based on id slug

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,12 +1,44 @@
 import Link from 'next/link';
 
 
-const Movie = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://imdb-api.com/en/API/Top250Movies/${process.env.IMDB_API_KEY}`);
+  const data = await res.json();
+
+  const paths = data.items.map(ninja => {
+      return {
+          params: {id: ninja.id.toString()}
+      }
+  })
+
+  return {
+      paths,
+      fallback: false
+  }
+
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://imdb-api.com/en/API/Trailer/${process.env.IMDB_API_KEY}/${id}`);
+  const data = await res.json();
+
+  return {
+      props: { movie: data }
+  }
+
+}
+
+
+const Details = ({movie}) => {
+  console.log({movie})
   return (
     <div className="container">
       <div className="movie-single">
         <div className="movie-single-title">
-          <h1>Page under construction</h1>
+          <h4>{movie.title}</h4>
+          <p>{movie.year}</p>
+          <p>{movie.videoTitle}</p>
         </div>
         <Link href="/">
           <a>Back to Homepage</a>
@@ -16,4 +48,4 @@ const Movie = () => {
   );
 }
  
-export default Movie;
+export default Details;
